6c6d3899db59bd1d8e97cddfd4dcf02f71b43e68
Meetu07 <114209655+MeetSherasiya@users.noreply.github.com>
2023-04-29 22:48:30 +0530

    delete

--- questionadmin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',index, name="index"),
-    path('additem/',Additem, name="Additem"),
-    path('delete/<str:id>/',delete, name="delete"),
-    path('edit/<str:id>/',edit, name="edit"),
-]
