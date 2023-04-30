5313ba640693cbd1cee40b5ec91ccd49ffc94ca8
Meetu07 <114209655+MeetSherasiya@users.noreply.github.com>
2023-04-30 13:15:46 +0530

    delete

--- questionadmin/decorators.py
+++
@@ -1,5 +0,0 @@
-from django.contrib.auth.decorators import user_passes_test
-
-def superuser_required(view_func):
-    decorated_view_func = user_passes_test(lambda user: user.is_superuser, login_url='login')
-    return decorated_view_func(view_func)
